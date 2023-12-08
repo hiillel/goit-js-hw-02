f40f8364687ab361c54f0371236afcd01c2e6fb0
hiillel <vicky.hillel26@gmail.com>
2023-12-08 01:24:12 +0100

    upd2

--- js/task-4.js
+++ js/task-4.js
@@ -1,30 +1,24 @@
-function getShippingCost(country){
-
-    let price;
-
-switch (country) {
+function getShippingCost(country) {
+  let price;
+  switch (country) {
     case "China":
-        price = 100;
-        return `Shipping to ${country} will cost ${price} credits`;
-        break;
+      price = 100;
+      break;
     case "Chile":
-        price = 250;
-        return `Shipping to ${country} will cost ${price} credits`;
-        break;
+      price = 250;
+      break;
     case "Australia":
-        price = 170;
-        return `Shipping to ${country} will cost ${price} credits`;
-        break;
+      price = 170;
+      break;
     case "Jamaica":
-        price = 120;
-        return `Shipping to ${country} will cost ${price} credits`;
-        break;
+      price = 120;
+      break;
     default:
-        return "Sorry, there is no delivery to your country";
-        break;
+      return "Sorry, there is no delivery to your country";
+      break;
+  }
+  return `Shipping to ${country} will cost ${price} credits`;
 }
-}
-
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
